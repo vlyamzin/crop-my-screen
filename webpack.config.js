2f2761b1e1effa7ff921a33ccd7d3c0fa0b34ce9
Vlad Lyamzin <vlad.lyamzin@avenga.com>
2022-01-20 17:59:08 +0200

    Release version 1.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const version = require('./package.json').version;
 const ESLintPlugin = require('eslint-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
-  const filename = isProduction ? 'crop-my-screen.min.js' : 'crop-my-screen.js';
+  const filename = isProduction ? `crop-my-screen.${version}.min.js` : `crop-my-screen.${version}.js`;
 
   return {
     entry: './src/index.js',
